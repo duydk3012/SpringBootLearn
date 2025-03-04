56e3ef56b526de36c3eca931d0ad913be59075cc
Duong Khanh Duy <duydk3012@gmail.com>
2025-03-04 09:29:59 +0700

    update 04-03-2025 p2

---
+++ 04-spring-boot-rest-crud/30-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/duydk/cruddemo/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.duydk.cruddemo.service;
+
+import com.duydk.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> findAll();
+
+    Employee findById(int id);
+
+    Employee save(Employee employee);
+
+    void deleteById(int id);
+}
---
+++ 04-spring-boot-rest-crud/30-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/duydk/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,61 @@
+package com.duydk.cruddemo.service;
+
+import com.duydk.cruddemo.dao.EmployeeRepository;
+import com.duydk.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return this.employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        Optional<Employee> result = this.employeeRepository.findById(id);
+
+        Employee employee = null;
+        if (result.isPresent()) {
+            employee = result.get();
+        } else {
+            throw new RuntimeException("Employee not found with id: " + id);
+        }
+        return employee;
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        employeeRepository.deleteById(id);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
