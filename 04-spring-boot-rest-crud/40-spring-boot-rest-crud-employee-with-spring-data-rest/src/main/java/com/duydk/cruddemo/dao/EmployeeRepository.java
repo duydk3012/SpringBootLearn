292f91dc1cb681c1bec6cce15c9defebcb87e685
Duong Khanh Duy <duydk3012@gmail.com>
2025-03-04 16:45:31 +0700

    04032025_3

--- 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/duydk/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/duydk/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.duydk.cruddemo.dao;
 
 import com.duydk.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
